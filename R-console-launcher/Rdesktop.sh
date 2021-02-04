d67e7fa23fcd98ba6ed855b55d9da0756017faa5
RishabhKumar5010 <74526647+RishabhKumar5010@users.noreply.github.com>
2021-02-04 19:50:49 +0530

    add comments for user

--- R-console-launcher/Rdesktop.sh
+++ R-console-launcher/Rdesktop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo Welcome $USER
+echo Welcome $USER    #remove this line if you don't want welcome user text
 
 #terminal title
-echo -ne "\033]0;R Statistical Computing\007"
+echo -ne "\033]0;R Statistical Computing\007"   #this line will be titlw of the the terminal window
 
-R --version | grep 'R version'
-echo "type q() to exit"
+R --version | grep 'R version'    #remove this if you don't want R version info
+echo "type q() to exit"   #remove this if you don't want this prompt
 echo
-R -q
+R -q  #this line executes R, if you want an interactive R session don't remove this line
